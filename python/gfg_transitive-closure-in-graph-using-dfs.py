a2fee51511ea5dde4d9738c32275bd637bc7bb98
adykumar <adykumar01@gmail.com>
2019-01-11 16:40:08 -0800

    gfg-transitive-closure-dfs

---
+++ python/gfg_transitive-closure-in-graph-using-dfs.py
@@ -0,0 +1,15 @@
+"""
+Transitive Closure of a Graph using DFS
+Given a directed graph, find out if a vertex v is reachable from another vertex u for all vertex pairs (u, v) in the given graph. Here reachable mean that there is a path from vertex u to v. The reach-ability matrix is called transitive closure of a graph.
+
+For example, consider below graph
+
+
+
+
+Transitive closure of above graphs is
+     1 1 1 1
+     1 1 1 1
+     1 1 1 1
+     0 0 0 1
+"""
